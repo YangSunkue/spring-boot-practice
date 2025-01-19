648b365d0166265d8fec452fb4447311380346a5
Yangsunkue <ysk9526@gmail.com>
2025-01-19 19:11:25 +0900

    feat: 프로젝트 생성 및 hello 페이지 추가

---
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -0,0 +1,19 @@
+package com.example.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	/**
+	 * 어플리케이션의 메인 메서드입니다.
+	 * Spring boot 애플리케이션을 초기화하고 실행합니다.
+	 *
+	 * @param args 명령행 인자
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+}
