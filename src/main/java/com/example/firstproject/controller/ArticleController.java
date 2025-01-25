b61ba9cdc18aa324c83142ae5ddf2c9df9270cb9
Yangsunkue <ysk9526@gmail.com>
2025-01-25 17:14:13 +0900

    feat: Dto를 엔티티로 변환, DI된 CrudRepository를 이용해 데이터 저장 및 확인

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     /**
      * 게시글 생성 페이지입니다.
@@ -25,6 +31,15 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        // 2. 리파지터리를 이용해 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "articles/new";
     }
 }
